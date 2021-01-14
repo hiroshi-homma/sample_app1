bfb0e139091fa418eac55984fbc4b6aedab50330
hiroshi-homma-origin <hiros.200063@gmail.com>
2021-01-15 02:48:27 +0900

    adjustment

--- feature/core/src/main/java/com/example/core/utils/BindingAdapter.kt
+++ feature/core/src/main/java/com/example/core/utils/BindingAdapter.kt
@@ -2,8 +2,6 @@ package com.example.core.utils
 
 import android.view.View
 import androidx.databinding.BindingAdapter
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.kotlin.project.data.model.MyNewsStatus
 
 @BindingAdapter("visibleGone")
 fun visibleGone(view: View, show: Boolean) {
@@ -13,10 +11,4 @@ fun visibleGone(view: View, show: Boolean) {
 @BindingAdapter("visibleInvisible")
 fun visibleInvisible(view: View, show: Boolean) {
     view.visibility = if (show) View.VISIBLE else View.VISIBLE
-}
-
-@BindingAdapter("isRefresh")
-fun SwipeRefreshLayout.isRefresh(timeLineStatus: MyNewsStatus?) {
-    timeLineStatus ?: return
-    isRefreshing = timeLineStatus == MyNewsStatus.RELOADING
 }
